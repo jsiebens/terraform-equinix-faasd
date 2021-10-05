995714c2959f5052092bbdda5cd7e94cb7a5907f
Johan Siebens <johan.siebens@gmail.com>
2021-10-05 13:03:32 +0200

    fix missing / in output

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "ipv4_address" {
 
 output "gateway_url" {
   description = "The url of the faasd gateway"
-  value       = var.domain == null || var.domain == "" ? format("http:/%s:8080", metal_device.faasd.access_public_ipv4) : format("https://%s", var.domain)
+  value       = var.domain == null || var.domain == "" ? format("http://%s:8080", metal_device.faasd.access_public_ipv4) : format("https://%s", var.domain)
 }
 
 output "basic_auth_user" {
